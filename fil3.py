3fa388ec4cab42573ffc09c94399651a5ea157f0
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-06-17 21:57:59 +0500

    Add files via upload

---
+++ fil3.py
@@ -0,0 +1,4 @@
+text=str(input("enter the string"))
+abc=text[0::2]
+#text=abc
+print("reverse string is",abc)
---
+++ file2.py
@@ -0,0 +1,22 @@
+#assigning different variables
+name="Pinguin"
+age=15
+is_student=True
+weight=38.5
+#printing different variables and their data type
+print("name:",name)
+print("data type of name is",type(name))
+print("age:",age)
+print("data type of age is",type(age))
+print("is_student:",is_student)
+print("data type of is_student is",type(is_student))
+print("weight:",weight)
+print("data type of weight is",type(weight))
+#type casting to convert the data type of variables
+print("\in after type casting----") 
+age=str(age)
+print(age)
+print("data type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("data type of weight is",type(weight))
